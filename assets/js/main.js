38147b848063785251aede370c619e4a2a5cc086
simon-svg <simonkarapetyan605@gmail.com>
2020-12-09 10:52:49 +0400

    added porsche page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,56 @@
+// preloader
+const preloader = document.querySelector(".preloader");
+const body = document.querySelector("body")
+document.body.onload = () => {
+    preloader.style.opacity = "0";
+    setTimeout(() => {
+        body.style["overflow-y"] = "scroll";
+        preloader.style.display = "none";
+        preloader.remove()
+    }, 500)
+}
+
+
+
+
+
+
+// header mobile
 const burgerMenu = document.querySelector(".burger-menu");
 const headerMobileNav = document.querySelector(".header-mobile__nav");
 let burgerMenuBool = true;
 
 burgerMenu.addEventListener("click", () => {
-    if(burgerMenuBool){
+    if (burgerMenuBool) {
         headerMobileNav.style.display = "flex";
     }
-    else{
+    else {
         headerMobileNav.style.display = "none";
     }
     burgerMenuBool = !burgerMenuBool;
+})
+
+
+
+
+
+const app = document.querySelector(".app");
+const mobileDevice = document.querySelector(".mobile-device");
+if (window.innerWidth < 900) {
+    app.style.display = "none";
+    mobileDevice.style.display = "block";
+}
+else {
+    app.style.display = "block";
+    mobileDevice.style.display = "none";
+}
+window.addEventListener("resize", (e) => {
+    if (window.innerWidth < 900) {
+        app.style.display = "none";
+        mobileDevice.style.display = "block";
+    }
+    else {
+        app.style.display = "block";
+        mobileDevice.style.display = "none";
+    }
 })
