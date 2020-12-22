35c27a785f9cd4bf1a3757a2e63b9081921eb8bc
simon-svg <simonkarapetyan605@gmail.com>
2020-12-22 21:03:37 +0400

    added voice mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,14 +20,25 @@ document.body.onload = () => {
 // header mobile
 const burgerMenu = document.querySelector(".burger-menu");
 const headerMobileNav = document.querySelector(".header-mobile__nav");
+const burgerMenuItem1 = document.querySelectorAll(".burger-menu__item")[0];
+const burgerMenuItem2 = document.querySelectorAll(".burger-menu__item")[1];
+const burgerMenuItem3 = document.querySelectorAll(".burger-menu__item")[2];
 let burgerMenuBool = true;
 
 burgerMenu.addEventListener("click", () => {
     if (burgerMenuBool) {
         headerMobileNav.style.display = "flex";
+        burgerMenuItem2.style.display = "none";
+        burgerMenuItem1.style.transform = "rotate(45deg) translate(2px, 3px)";
+        burgerMenuItem3.style.transform = "rotate(-45deg)";
+        burgerMenu.style["justify-content"] = "center";
     }
     else {
+        burgerMenu.style["justify-content"] = "space-between";
         headerMobileNav.style.display = "none";
+        burgerMenuItem2.style.display = "flex";
+        burgerMenuItem1.style.transform = "none";
+        burgerMenuItem3.style.transform = "none";
     }
     burgerMenuBool = !burgerMenuBool;
 })
@@ -55,4 +66,58 @@ window.addEventListener("resize", (e) => {
         app.style.display = "block";
         mobileDevice.style.display = "none";
     }
+})
+
+
+
+
+
+
+
+
+
+
+// header navigation button
+const headerBtn = document.querySelector(".header__btn");
+const recognizer = new webkitSpeechRecognition();
+recognizer.interimResults = true;
+recognizer.lang = 'en-US';
+let all = "";
+const synth = window.speechSynthesis;
+
+
+recognizer.onresult = function (event) {
+    let result = event.results[event.resultIndex];
+    all = result[0].transcript;
+    if (result.isFinal) {
+        if (result[0].transcript == "bmw" || result[0].transcript == "BMW" || result[0].transcript == "Bmw") {
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/bmw.html")
+        }
+        else if (result[0].transcript == "Methodist" || result[0].transcript == "Mercedes" || result[0].transcript == "mercedes" ||
+        result[0].transcript == "Merced this") {
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/mercedes.html")
+        }
+        else if (result[0].transcript == "Porsche" || result[0].transcript == "borscht") {
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/porsche.html")
+        }
+        else if (result[0].transcript == "chevrolet" || result[0].transcript == "Chevrolet" || result[0].transcript == "Chevron.") {
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/chevrolet.html")
+        }
+        else if (result[0].transcript == "coach" || result[0].transcript == "Doge") {
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/dodge.html")
+        }
+        else if (result[0].transcript == "smok tank" || result[0].transcript == "smooth tongue" || result[0].transcript == "move tongue" || result[0].transcript == "most tongue" || result[0].transcript == "Mustang") {
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/mustang.html")
+        }
+        else {
+            const utterance1 = new SpeechSynthesisUtterance(`sorry you say ${all}`);
+            synth.speak(utterance1);
+        }
+    }
+    else {
+        console.log('result ', result[0].transcript);
+    }
+};
+headerBtn.addEventListener("click", () => {
+    recognizer.start();
 })
