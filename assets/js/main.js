ec78c3bf3967426224f05232a08bc3627883d511
simon-svg <simonkarapetyan605@gmail.com>
2020-12-23 15:19:27 +0400

    added sound navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,36 @@
 // preloader
 const preloader = document.querySelector(".preloader");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
+const musicBmw = document.querySelector(".music-bmw");
 document.body.onload = () => {
     preloader.style.opacity = "0";
     setTimeout(() => {
         if (document.title !== "Cars Evolution") {
             body.style["overflow-y"] = "scroll";
         }
+        if (document.title == "BMW") {
+            musicBmw.play();
+        }
         preloader.style.display = "none";
         preloader.remove()
     }, 500)
 }
+
+
+
+
+
+
+// header link event
+const headerLinks = document.querySelectorAll(".header__link");
+headerLinks.forEach((item) => {
+    item.addEventListener("click", () => {
+        audio.play();
+        setTimeout(() => {
+            audio.pause();
+        }, 3250)
+    })
+})
 
 
 
@@ -84,6 +104,7 @@ recognizer.interimResults = true;
 recognizer.lang = 'en-US';
 let all = "";
 const synth = window.speechSynthesis;
+const audio = document.querySelector(".audio");
 
 
 recognizer.onresult = function (event) {
@@ -91,23 +112,47 @@ recognizer.onresult = function (event) {
     all = result[0].transcript;
     if (result.isFinal) {
         if (result[0].transcript == "bmw" || result[0].transcript == "BMW" || result[0].transcript == "Bmw") {
+            audio.play();
             window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/bmw.html")
+            setTimeout(() => {
+                audio.pause();
+            }, 3250)
         }
         else if (result[0].transcript == "Methodist" || result[0].transcript == "Mercedes" || result[0].transcript == "mercedes" ||
-        result[0].transcript == "Merced this") {
-            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/mercedes.html")
+            result[0].transcript == "Merced this") {
+            audio.play();
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/mercedes.html");
+            setTimeout(() => {
+                audio.pause();
+            }, 3250);
         }
         else if (result[0].transcript == "Porsche" || result[0].transcript == "borscht") {
-            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/porsche.html")
+            audio.play();
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/porsche.html");
+            setTimeout(() => {
+                audio.pause();
+            }, 3250);
         }
         else if (result[0].transcript == "chevrolet" || result[0].transcript == "Chevrolet" || result[0].transcript == "Chevron.") {
-            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/chevrolet.html")
+            audio.play();
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/chevrolet.html");
+            setTimeout(() => {
+                audio.pause();
+            }, 3250);
         }
         else if (result[0].transcript == "coach" || result[0].transcript == "Doge") {
-            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/dodge.html")
+            audio.play();
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/dodge.html");
+            setTimeout(() => {
+                audio.pause();
+            }, 3250);
         }
         else if (result[0].transcript == "smok tank" || result[0].transcript == "smooth tongue" || result[0].transcript == "move tongue" || result[0].transcript == "most tongue" || result[0].transcript == "Mustang") {
-            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/mustang.html")
+            audio.play();
+            window.open("https://simon-svg.github.io/CarsEvolution/assets/htmls/mustang.html");
+            setTimeout(() => {
+                audio.pause();
+            }, 3250);
         }
         else {
             const utterance1 = new SpeechSynthesisUtterance(`sorry you say ${all}`);
